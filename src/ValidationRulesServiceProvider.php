df6382a1369630e0a1a72002f5ceaf13e2093b79
Valentino Lauciani <valentino.lauciani@ingv.it>
2022-04-06 17:30:37 +0200

    minor fix

--- src/ValidationRulesServiceProvider.php
+++ src/ValidationRulesServiceProvider.php
@@ -8,12 +8,6 @@ class ValidationRulesServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-	/*
-        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang/', 'validationRules');
 
-        $this->publishes([
-            __DIR__ . '/../resources/lang' => resource_path('lang/vendor/validationRules'),
-        ]);
-	*/
     }
 }
