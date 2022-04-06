f9a543237427b0e6331ede21f21c3eded5cb8ad4
Valentino Lauciani <valentino.lauciani@ingv.it>
2022-04-06 17:01:13 +0200

    Initial commit

---
+++ tests/Rules/RFC3339ExtendedTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace VLauciani\LaravelValidationRules\Tests\Rules;
+
+use VLauciani\LaravelValidationRules\Tests\TestCase;
+use VLauciani\LaravelValidationRules\Rules\RFC3339ExtendedRule;
+
+class RFC3339ExtendedTest extends TestCase
+{
+    public function testRulePasses()
+    {
+        $rule = new RFC3339ExtendedRule();
+        $this->assertTrue($rule->passes('', '2021-10-06T06:33:36.440+00:00'));
+        $this->assertTrue($rule->passes('', '2021-10-06T06:33:36.440Z'));
+        $this->assertTrue($rule->passes('', '2021-10-06T06:33:36Z'));
+    }
+
+    public function testRuleFails()
+    {
+        $rule = new RFC3339ExtendedRule();
+        $this->assertFalse($rule->passes('', 'I\'m not a valid RFC3339Extended!'));
+    }
+}
