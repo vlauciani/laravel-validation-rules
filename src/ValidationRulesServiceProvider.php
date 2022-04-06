51572477c451b5c422e4d763364d24918182e34e
Valentino Lauciani <valentino.lauciani@ingv.it>
2022-04-06 17:29:33 +0200

    Removing publish resource lang

--- src/ValidationRulesServiceProvider.php
+++ src/ValidationRulesServiceProvider.php
@@ -8,10 +8,12 @@ class ValidationRulesServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+	/*
         $this->loadTranslationsFrom(__DIR__ . '/../resources/lang/', 'validationRules');
 
         $this->publishes([
             __DIR__ . '/../resources/lang' => resource_path('lang/vendor/validationRules'),
         ]);
+	*/
     }
 }
